452cfe8ecdf42bfd08003f505f9f94dc3dc63fc5
Priyangsu Banerjee <priyangsu26@gmail.com>
2024-03-09 03:06:43 +0530

    Add redirect to external URL

---
+++ app/kolosseum/page.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+function Page() {
+  return redirect("https://kolosseum.konnexions.dev");
+  return <div>page</div>;
+}
+
+export default Page;
--- app/page.js
+++ app/page.js
@@ -1,6 +1,8 @@
 import Image from "next/image";
+import { redirect } from "next/navigation";
 
 export default function Home() {
+  return redirect("https://kolosseum.konnexions.dev");
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
